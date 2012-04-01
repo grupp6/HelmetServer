e1fa8d7a4a1f5a3902a20434feebb8dd09951e4f
Kristofer Leifland <peoplesbeer@gmail.com>
2012-04-01 18:33:02 +0200

    Created HibernateUtil.java.

---
+++ src/se/mah/helmet/server/hibernate/HibernateUtil.java
@@ -0,0 +1,22 @@
+package se.mah.helmet.server.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		configuration.setProperty("connection.password", "mycqlketlet");
+		final ServiceRegistry serviceRegistry = 
+				new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+		return configuration.buildSessionFactory(serviceRegistry);
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
